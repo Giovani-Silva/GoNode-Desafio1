e2e8c264d7a18bf27def52690ad126548f48c62a
Giovani Silva <giovani.silva@live.com>
2018-11-20 13:42:37 -0200

    Desafio complete

--- index.js
+++ index.js
@@ -9,15 +9,58 @@ nunjucks.configure('views', {
   watch: true
 });
 
+app.set('view engine', 'njk');
+
 app.use(
   express.urlencoded({
     extended: false
   })
 )
-app.set('view engine', 'njk')
+
+const checkParams = (req, res, next) => {
+  if (!req.query.age) return res.redirect('/');
+  return next();
+}
+
+const MinorMajor = (req, res, next) => {
+  const view = req.query.age > 18 ? '/major' : '/minor';
+
+  if (view !== req.path) return res.redirect(`${view}?age=${req.query.age}`);
+
+  return next();
+};
+
+const checkAge = age => {
+  if (-age && age > 0) {
+    return age;
+  }
+  return false;
+};
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
+  res.render('main');
+});
 
-app.listen(3000)
+app.get('/major', checkParams, MinorMajor, (req, res) => {
+  res.render('major', {
+    age: req.query.age
+  });
+});
+
+
+app.get('/minor', checkParams, MinorMajor, (req, res) => {
+  res.render('minor', {
+    age: req.query.age
+  });
+});
+
+app.post('/check', (req, res) => {
+  const age = checkAge(req.body.age);
+  if (age) {
+    const view = age > 18 ? '/major' : '/minor';
+    return res.redirect(`${view}?age=${age}`);
+  }
+  return res.redirect('/');
+});
+
+app.listen(3000);
