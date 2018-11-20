d8b7f3b061d60d5f8643399ae5183068bb09f85c
Giovani Silva <giovani.silva@live.com>
2018-11-20 12:45:38 -0200

    Node Starter - ExpressJS, Nunjucks, EditorConfig e ESLint, Nodemon

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+});
+
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+)
+app.set('view engine', 'njk')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(3000)
